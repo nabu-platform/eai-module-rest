4ff390f4992d9581511d82627851778bd4dc9eb9
alex <alex@nabu.be>
2016-10-05 15:44:20 +0200

    Added methods

--- src/main/java/be/nabu/eai/module/rest/WebMethod.java
+++ src/main/java/be/nabu/eai/module/rest/WebMethod.java
@@ -1,5 +1,5 @@
 package be.nabu.eai.module.rest;
 
 public enum WebMethod {
-	GET, POST, PUT, DELETE
+	GET, POST, PUT, DELETE, PATCH, HEAD, TRACE, OPTIONS, CONNECT, COPY, MOVE, LOCK, UNLOCK, MKCOL, PROPPATCH, PROPFIND
 }
